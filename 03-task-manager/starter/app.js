913c1333b618fb1162d535345f53e34bef14d2fe
ivan.hryshko <ihryshko@datrics.ai>
2023-01-21 22:52:42 +0200

    feat: added start method

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,8 +1,7 @@
-require('./db/connect')
 const express = require('express')
 const app = express();
-
 const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
 
 // middleware
 
@@ -17,6 +16,13 @@ app.use('/api/v1/tasks', tasks)
 
 const port = 3001
 
-app.listen(port, console.log(`Server is listeting on port: ${port}...`))
+const start = async () => {
+  try {
+    connectDB()
+    app.listen(port, console.log(`Server is listeting on port: ${port}...`))
+  } catch (error) {
+    console.log(error);
+  }
+}
 
-
+start()
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 const connectionString =
   'mongodb+srv://giver0:12345@cluster0.8oohz6f.mongodb.net/?retryWrites=true&w=majority'
 
-  mongoose.connect(connectionString, {
+const connectDB = (url) => {
+  return mongoose.connect(connectionString, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log('CONNECTED TO DB...'))
-  .catch((error) => console.log('error >> ', error))
+}
+
+module.exports = connectDB
+
